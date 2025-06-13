1c6db7b9211ae42815b89bbf980bc368e29279ee
Shaheel kumar <heelixkumar98@gmail.com>
2025-06-14 03:06:40 +1200

    update

--- whatsapp_webhook/webhook.py
+++ whatsapp_webhook/webhook.py
@@ -11,7 +11,7 @@ from core_logic.ocr import extract_text_from_image
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 ENV_PATH = os.path.join(BASE_DIR, '.env')
 
-load_dotenv(dotenv_path=ENV_PATH)
+load_dotenv()
 app = FastAPI()
 
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
